024e9be49b8c49e4c7a8de0abb4e7feef1d50b41
Yann <106726503+YannGB-c@users.noreply.github.com>
2025-08-27 11:19:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
+function ExcusesGenerator() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+  let randomwhos = who[Math.floor(Math.random() * who.length)];
+  let randomverbs = action[Math.floor(Math.random() * action.length)];
+  let randomwhats = what[Math.floor(Math.random() * what.length)];
+  let randomtiming = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let excuse = `${randomwhos} ${randomverbs} ${randomwhats} ${randomtiming}`;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuse;
+
+  console.log(randomwhos, randomverbs, randomwhats, randomtiming);
 };
+
+// Escuchar el evento "click" en el botón
+document
+  .getElementById("generate-button")
+  .addEventListener("click", ExcusesGenerator);
+
+// Llamar a la función una vez al cargar la página para que no esté vacía.
+ExcusesGenerator();
